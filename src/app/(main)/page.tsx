4165a69ae6ecf603cbf6b97a18c524ef6a931766
ayush rawat <ayushrawat324@gmail.com>
2023-10-30 23:50:58 +0530

    refactor code

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -10,7 +10,7 @@ import Footer from "@/components/Footer";
 export default function Home() {
   return (
     <>
-      <Navbar></Navbar>
+      <Navbar admin={false}></Navbar>
       <div className="container mx-auto flex px-5 py-10 md:flex-row flex-col ">
         <div className="lg:flex-grow  lg:w-10 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
           <Main></Main>
